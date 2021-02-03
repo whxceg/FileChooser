9352ecaa8450b23a665500b35efd35b1bb8857d7
wanghao <751237116@qq.com>
2021-02-03 09:45:02 +0800

    init

---
+++ Sample/src/main/java/group/pals/android/lib/ui/filechoose/sample/MainActivity.java
@@ -0,0 +1,32 @@
+package group.pals.android.lib.ui.filechoose.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileChooserUtil.runFileChooser(MainActivity.this, 888, "选择文件", "/", null);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("TAG", "onActivityResult: " + requestCode + "  ,resultCode:" + resultCode);
+    }
+}
